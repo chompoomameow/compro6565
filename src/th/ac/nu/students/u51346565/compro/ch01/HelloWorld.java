d1d2c5aec00b8566a51e9a8376ad16dd67afa494
chompoomameow <yamm-oon_bie@hotmail.com>
2012-06-25 21:16:29 +0700

    HelloWorld

---
+++ src/th/ac/nu/students/u51346565/compro/ch01/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.students.u51346565.compro.ch01;
+
+/**
+ *
+ * @author Mameaw
+ */
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("Hello,World!");
+    }
+}
