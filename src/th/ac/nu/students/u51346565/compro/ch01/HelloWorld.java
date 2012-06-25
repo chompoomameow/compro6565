81fc2d96ab6e8ce49430bae71a7eb3f39bc4777a
chompoomameow <yamm-oon_bie@hotmail.com>
2012-06-25 21:19:59 +0700

    เพิ่ม Programming is Fun!!

--- src/th/ac/nu/students/u51346565/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u51346565/compro/ch01/HelloWorld.java
@@ -11,5 +11,6 @@ package th.ac.nu.students.u51346565.compro.ch01;
 public class HelloWorld {
     public static void main (String [] args){
         System.out.println("Hello,World!");
+        System.out.println("Programming is Fun!!");
     }
 }
