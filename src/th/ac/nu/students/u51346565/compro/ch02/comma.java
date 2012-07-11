0c76d34c38d8fa25726031c7ab966939bb7a5f3f
chompoomameow <yamm-oon_bie@hotmail.com>
2012-07-11 23:35:32 +0700

    homework02

---
+++ src/th/ac/nu/students/u51346565/compro/ch02/comma.java
@@ -0,0 +1,15 @@
+
+package th.ac.nu.students.u51346565.compro.ch02;
+
+import java.util.Scanner;
+
+public class comma {
+        public static void main(String[]args){
+    Scanner in = new Scanner(System.in);
+      System.out.println("Enter an integer between 1000 to 999,999:");
+      String number = in.nextLine();
+      int length = number.length();
+      System.out.print(number.substring(0, length -4)+number.substring(length -3, length));
+}
+}
+
---
+++ src/th/ac/nu/students/u51346565/compro/ch02/rectangle.java
@@ -0,0 +1,19 @@
+
+package th.ac.nu.students.u51346565.compro.ch02;
+
+import java.util.Scanner;
+
+public class rectangle {
+    
+public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Width");
+        double w = in.nextDouble();
+        System.out.println("Enter Long");
+        double l = in.nextDouble();
+        System.out.println("Area:" +w*l);
+        System.out.println("perimeter:" +(2*(w+1)));
+        System.out.println("diagonal:" +Math.sqrt((w*w+l*1)));
+    }
+}
+
